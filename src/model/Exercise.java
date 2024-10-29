0581d64deca79c167037637536d8025750f69c9b
Frederick Neimeister <pakaday@gmail.com>
2024-10-29 14:41:15 -0400

    Exercise class created

---
+++ src/model/Exercise.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Exercise {
+    private String name;
+    private int duration;
+    private double caloriesBurned;
+
+    public Exercise(String name, int duration, double caloriesBurned) {
+        this.name = name;
+        this.duration = duration;
+        this.caloriesBurned = caloriesBurned;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public double getCaloriesBurned() {
+        return caloriesBurned;
+    }
+
+    public void displayExerciseInfo() {
+        System.out.println("Exercise Name: " + name);
+        System.out.println("Duration: " + duration);
+        System.out.println("Calories burned: " + caloriesBurned + " kcal");
+    }
+}
