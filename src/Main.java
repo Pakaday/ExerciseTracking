2d927b4433764e04ff255263a6efd8cf5b9868b6
Frederick Neimeister <pakaday@gmail.com>
2024-10-29 14:41:01 -0400

    Main updated for User profile

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import model.User;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Enter your weight in pounds: ");
+        double weight = scanner.nextDouble();
+
+        System.out.println("Enter your height in inches: ");
+        double height = scanner.nextDouble();
+
+        // Create a new User
+        User user = new User(name, age, weight, height);
+
+        // Display user profile info
+        user.displayUserInfo();
+
+        scanner.close();
     }
 }
