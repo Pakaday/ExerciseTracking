318171cc44be9c666dc93e3ef34b366d7413796a
Frederick Neimeister <pakaday@gmail.com>
2024-10-30 08:40:31 -0400

    added ActivityLog class

--- src/model/ActivityLog.java
+++ src/model/ActivityLog.java
@@ -1,4 +1,32 @@
 package model;
 
+import java.util.ArrayList;
+
 public class ActivityLog {
+    private ArrayList<Exercise> exercises;
+
+    public ActivityLog() {
+        this.exercises = new ArrayList<>();
+    }
+
+    public void addExercise(Exercise exercise) {
+        exercises.add(exercise);
+        System.out.println("Exercise added to log.");
+    }
+
+    public void displayExercise() {
+        if (exercises.isEmpty()) {
+            System.out.println("No exercises logged.");
+        } else {
+            System.out.println("Exercise Log:");
+            for (Exercise exercise : exercises) {
+                exercise.displayExerciseInfo();
+                System.out.println();
+            }
+        }
+    }
+
+    public ArrayList<Exercise> getExercises() {
+        return exercises;
+    }
 }
